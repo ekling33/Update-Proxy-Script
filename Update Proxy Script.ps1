ab6a761a11b45782891b2efb654ed3afad0cb5a9
ekling33 <eric.klingerman@gmail.com>
2026-03-10 14:31:20 -0400

    Update Update Proxy Script.ps1

--- Update Proxy Script.ps1
+++ Update Proxy Script.ps1
@@ -1,1 +1,7 @@
-Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' | Select BuildLabEx, UBR, CurrentBuild
+reagentc /disable
+$WinREPath = (reagentc /info | Select-String "Windows RE location").Line.Split(':')[1].Trim() + ':\Recovery\WindowsRE\winre.wim'
+mkdir C:\winremount -Force
+dism /Mount-Image /ImageFile:$WinREPath /Index:1 /MountDir:C:\winremount
+dism /Image:C:\winremount /Add-Package /PackagePath:"C:\Downloads\windows10.0-kb5034232-x64.cab"
+dism /Unmount-Image /MountDir:C:\winremount /Commit
+reagentc /enable
