9e446dc24d9e7c082325fe0b58b40100cc94e041
ekling33 <eric.klingerman@gmail.com>
2026-04-02 16:42:29 -0400

    Update Update Proxy Script.ps1

--- Update Proxy Script.ps1
+++ Update Proxy Script.ps1
@@ -1,11 +1,5 @@
-$paths = @(
-  "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319",
-  "HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319"
-)
+# RD cert issues
+Get-WmiObject Win32_TerminalServiceSetting | Select-Object SSLCertificateSHA1Hash
 
-foreach ($path in $paths) {
-  New-Item $path -Force -ErrorAction SilentlyContinue | Out-Null
-  Set-ItemProperty $path -Name "SchUseStrongCrypto" -Value 1 -Type DWord -Force
-  Set-ItemProperty $path -Name "SystemDefaultTlsVersions" -Value 1 -Type DWord -Force
-  Write-Host "Fixed $path"
-}
+# Event 1064 details
+Get-WinEvent -FilterHashtable @{LogName='System'; ID=1064} -MaxEvents 1 | Select TimeCreated, Message
