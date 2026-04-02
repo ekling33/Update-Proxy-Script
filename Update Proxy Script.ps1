dc4a78f593eb8928173efc7998c5e119c7e7435f
ekling33 <eric.klingerman@gmail.com>
2026-04-02 15:54:14 -0400

    Update Update Proxy Script.ps1

--- Update Proxy Script.ps1
+++ Update Proxy Script.ps1
@@ -1,14 +1,11 @@
-# .NET TLS 1.2 modern ciphers (KB5078752 compatible)
 $paths = @(
   "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319",
   "HKLM:\SOFTWARE\WOW6432Node\Microsoft\.NETFramework\v4.0.30319"
 )
 
 foreach ($path in $paths) {
-  if (!(Test-Path $path)) { New-Item $path -Force | Out-Null }
+  New-Item $path -Force -ErrorAction SilentlyContinue | Out-Null
   Set-ItemProperty $path -Name "SchUseStrongCrypto" -Value 1 -Type DWord -Force
   Set-ItemProperty $path -Name "SystemDefaultTlsVersions" -Value 1 -Type DWord -Force
+  Write-Host "Fixed $path"
 }
-
-gpupdate /force
-Write-Host ".NET strong crypto enabled. Restart Runtime Resource."
