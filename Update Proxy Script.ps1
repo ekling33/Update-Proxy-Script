af14145af1e2bb0f46a353b3d26ddc9f14779ec8
ekling33 <eric.klingerman@gmail.com>
2026-04-02 14:22:42 -0400

    Update Update Proxy Script.ps1

--- Update Proxy Script.ps1
+++ Update Proxy Script.ps1
@@ -1,6 +1,8 @@
-  <!-- existing content -->
-  <appSettings>
-    <add key="WebSocketSendTimeout" value="00:00:30" />
-    <add key="WebSocketReceiveTimeout" value="00:00:30" />
-  </appSettings>
-</config>
+# List all certs in Personal store with thumbprint, subject, expiry
+Get-ChildItem Cert:\LocalMachine\My | 
+  Select-Object Thumbprint, Subject, NotAfter, Issuer |
+  Sort-Object NotAfter |
+  Format-Table -AutoSize
+
+# Export cert list to file for comparison
+Get-ChildItem Cert:\LocalMachine\My | Export-Clixml "C:\BP_Certs_$env:COMPUTERNAME.xml"
