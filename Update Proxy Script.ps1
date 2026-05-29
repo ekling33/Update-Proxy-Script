14f1bda8179ed1bc13dab87ac8e36a9661aec0f8
ekling33 <eric.klingerman@gmail.com>
2026-05-29 09:40:29 -0400

    Update Update Proxy Script.ps1

--- Update Proxy Script.ps1
+++ Update Proxy Script.ps1
@@ -1,3 +1,7 @@
 $err = $null
 Update-MpSignature -Verbose -ErrorAction Continue -ErrorVariable err 2>&1 | Tee-Object C:\Temp\defender-update.log
 $err
+
+
+
+Get-MpComputerStatus | Select-Object AMProductVersion,AntivirusSignatureVersion,AntivirusSignatureLastUpdated | Format-List
