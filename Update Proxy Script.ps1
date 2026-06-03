baf9ac31067784c5cd180f96760a7faae87fd699
ekling33 <eric.klingerman@gmail.com>
2026-06-03 12:30:28 -0400

    Update Update Proxy Script.ps1

--- Update Proxy Script.ps1
+++ Update Proxy Script.ps1
@@ -1,2 +1,15 @@
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" /v DefaultSecureProtocols /t REG_DWORD /d 0x00000C00 /f
-reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings\WinHttp" /v DefaultSecureProtocols /t REG_DWORD /d 0x00000C00 /f
+\$pathClient = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client"
+\$pathServer = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Server"
+
+foreach (\$p in @(\$pathClient, \$pathServer)) {
+  if (-not (Test-Path \$p)) { New-Item -Path \$p -Force | Out-Null }
+  Set-ItemProperty -Path \$p -Name "Enabled"    -Value 0 -Type DWord
+  Set-ItemProperty -Path \$p -Name "DisabledByDefault" -Value 1 -Type DWord
+}
+
+Restart-Computer -Force
+
+################################################################################
+
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value 1 -Type DWord
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value 1 -Type DWord
